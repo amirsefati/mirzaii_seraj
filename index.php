3c58671fc4cc01562361f81835819717e312d642
amir <sefati100@gmail.com>
2021-02-10 15:35:27 +0330

    try 3

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+
+        <div style="height:70vh;width:80%;margin:auto">
+        <iframe src="./pdf.php" width="100%" height="100%" frameborder="0"></iframe>
+        </div> 
+  </body>
+</html>
+
+
